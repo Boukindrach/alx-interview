9cbf5e7d6094026837497464a144890f4fa2897f
Boukind Rachid <boukind.r1@gmail.com>
2024-10-03 01:52:49 -0700

    a much better and clean way for the algorithem

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,44 +1,19 @@
 #!/usr/bin/python3
 
-array2 = [1, 5, 10, 10, 5, 1]
+def pascal_triangle(n):
+    if n <= 0:
+        return []
 
-"""if len(array) % 2 == 1:
-    length = int (len(array) - 1 / 2)
-    number = 0
-    array1 = []
-    n = 0
-    o = 0
-    for i in range(0, length):
-        n -= o
-        number += array[i]
-        array1.append(number)
-        n += 1
-        if n == 2:
-            number = array[i]
-            o = 1
-        if array[i] > array[i + 1]:
-            break
-    array2 = array1[::-1]
-    total_array = array1 + array2
-    print(total_array)
-"""
-if len(array2) % 2 == 0:
-    length = int (len(array2) - 1 / 2)
-    number = 0
-    array1 = []
-    n = 0
-    o = 0
-    print(length)
-    for i in range(0, length):
-        n -= o
-        number += array2[i]
-        array1.append(number)
-        n += 1
-        if n == 2:
-            number = array2[i]
-            o = 1
-        if array2[i] > array2[i + 1]:
-            break
-    array2 = array1[-2::-1]
-    total_array = array1 + array2
-    print(total_array)
+    triangle = [[1]]
+
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        new_row = [1]
+
+        for j in range(1, len(prev_row)):
+            new_row.append(prev_row[j-1] + prev_row[j])
+
+        new_row.append(1)
+        triangle.append(new_row)
+
+    return triangle
